229b86887ebf94c1c88bc85c8e4597596a87fbc7
Mehdi Osman <mehdi@asayer.io>
2021-04-30 01:36:37 +0200

    Trying algolia

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ import Loadable from 'react-loadable';
 import LoadingProvider from './mdxComponents/loading';
 
 const help = require('./images/help.svg');
-const isSearchEnabled = config.header.search && config.header.search.enabled ? true : false;
+const isSearchEnabled = config.header.search && config.header.search.enabled ? true : true;
 
 let searchIndices = [];
 if(isSearchEnabled && config.header.search.indexName) {
