38db45530e3c188ee21d9d478f078b038a5ee925
Mehdi Osman <mehdi@asayer.io>
2021-05-01 14:23:23 +0200

    Fix digital ocean

--- config.js
+++ config.js
@@ -50,10 +50,10 @@ const config = {
 	"siteMetadata": {
 		"title": "OpenReplay Documentation",
 		"description": "The one stop location for tutorials, guides, and information about OpenReplay.",
-		"ogImage": "static/media/og_image.png",
+		"ogImage": "https://docs.openreplay.com/static/og_image.png",
 		"ogSiteName": "OpenReplay",
 		"docsLocation": "https://github.com/openreplay/documentation",
-		"favicon": "static/media/favicon.png"
+		"favicon": "https://docs.openreplay.com/static/favicon.png"
 	},
 	"pwa": {
 		"enabled": false, // disabling this will also remove the existing service worker.
--- content/deployment/deploy-digitalocean.md
+++ content/deployment/deploy-digitalocean.md
@@ -23,7 +23,7 @@ OpenReplay stack can be installed on a single machine (droplet) and Digital Ocea
 SSH_KEY=~/Downloads/openreplay-key.pem ## wherever you've saved the SSH key
 INSTANCE_IP=REPLACE_WITH_DROPLET_PUBLIC_IP
 chmod 400 $SSH_KEY
-ssh -i $SSH_KEY ubuntu@$INSTANCE_IP
+ssh -i $SSH_KEY root@$INSTANCE_IP
 ```
 
 2. Install OpenReplay:
