1b249a1875f8f62a4bed0ea80d57cf56911818a3
Fernando Doglio <fernando@openreplay.com>
2022-09-20 12:11:59 +0200

    fixing styles for version dropdown

--- src/components/versionSelector.js
+++ src/components/versionSelector.js
@@ -7,6 +7,25 @@ const VERSIONS = [
     {title: "1.8.0", url: ""}
 ]
 const Container = styled.div`
+    background: #eee;
+    padding: 8px 10px;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+
+    select {
+        font-size: 17px;
+		    border-radius: 4px;
+		    outline: none;
+		    border: none;
+		    width: 100%;
+		    font-family: "Source Sans Pro",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif;
+		   background: transparent;
+		    padding-right: 1rem;
+    }
+
         h3 {
             padding: 5px;
         }
@@ -57,14 +76,13 @@ export default function VersionSelector() {
 
     return (
         <Container>
-            <p>Version: 
             <select onChange={
                 updateLocation
             }>
                 {VERSIONS.map( v => {
-                    return <option value={ v.url } selected={v.url.indexOf(currentVersion) != -1}>V {v.title}</option>
+                    return <option value={ v.url } selected={v.url.indexOf(currentVersion) != -1}>v {v.title}</option>
                 })}
-            </select></p>
+            </select>
         </Container>
     )
 }
