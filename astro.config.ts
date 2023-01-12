90fd7567045cf020778315ff8013677ba68bfa92
Sudheer Salavadi <connect.uxmaster@gmail.com>
2023-01-12 15:54:05 +0100

    Multiple updates

--- astro.config.ts
+++ astro.config.ts
@@ -55,13 +55,7 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
-      theme: {
-        name: 'Star gazer',
-        type: 'dark',
-        settings: tokens,
-        fg: foregroundPrimary,
-        bg: backgroundPrimary
-      }
+      theme: 'material-darker'
     },
     remarkPlugins: [remarkCodeSnippets()]
   },
