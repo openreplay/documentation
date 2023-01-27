ccf431670171a04e5b61c28294bc68f8e020e5b6
Fernando Doglio <fernando@openreplay.com>
2023-01-27 21:39:49 +0100

    adding components for the landing page, restructuring of the left-side menu

--- src/i18n/en/nav.ts
+++ src/i18n/en/nav.ts
@@ -26,16 +26,14 @@ export default [
 	{ text: 'OpenReplay Administration', slug: 'deployment/openreplay-admin', key: 'deploy-aws' },
 	{ text: 'Upgrade Deployment', slug: 'deployment/upgrade', key: 'migrate' },
 
-	{ text: 'Installation', slug: 'installation/', header: true, type: 'learn', key: 'installation', icon: 'installation' },
-	{ text: 'Overview', slug: 'installation', key: 'blog-tutorial' },
-	{ text: 'Custom Events', slug: 'installation/custom-events', key: 'blog-tutorial' },
-	{ text: 'Error reporting', slug: 'installation/error-reporting', key: 'blog-installation' },
-	{ text: 'Identify a User', slug: 'installation/identify-user', key: 'blog-installation' },
-	{ text: 'JavaScript SDK', slug: 'installation/javascript-sdk', key: 'blog-installation' }, 
-	{ text: 'Metadata', slug: 'installation/metadata', key: 'blog-installation' },
-	{ text: 'Sanitize Data', slug: 'installation/sanitize-data', key: 'blog-installation' },
-	{ text: 'Setup OpenReplay', slug: 'installation/setup-or', key: 'blog-installation' },
-	{ text: 'Upload Source maps', slug: 'installation/upload-sourcemaps', key: 'blog-installation' },
+	{ text: 'Using OpenReplay with...', slug: 'using-or/', header: true, type: 'learn', key: 'installation', icon: 'installation' },
+	{ text: 'Overview', slug: 'using-or', key: 'tutorials/' },
+	{ text: '... Next.js', slug: 'using-or/next', key: 'using-or/' },
+	{ text: '... Nuxt', slug: 'using-or/nuxt', key: 'using-or/' },
+	{ text: '... Remix', slug: 'using-or/remix', key: 'using-or/' },
+	{ text: '... Svelte', slug: 'using-or/svelte', key: 'using-or/' },
+	{ text: '... Angular', slug: 'using-or/angular', key: 'using-or/' },
+	{ text: '... Gatsby', slug: 'using-or/gatsby', key: 'using-or/' },
 
 	{ text: 'Configuration', slug: 'configuration/', header: true, type: 'learn', key: 'configuration', icon: 'configure' },
 	{ text: 'Overview', slug: 'configuration', key: 'concepts/why-astro' },
@@ -47,37 +45,17 @@ export default [
 	{ text: 'Secure OpenReplay', slug: 'configuration/secure-or', key: 'concepts/islands' },
 	{ text: 'Single Sign-On (SSO)', slug: 'configuration/sso', key: 'concepts/islands' },
 
-	//tutorials
-	{ text: 'Tutorials', slug: 'tutorials/', header: true, type: 'learn', key: 'tutorials', icon: 'tutorials' },
-	//about plugins
-	{ text: 'Overview', slug: 'tutorials', key: 'core-concepts/layouts' },
-	{ text: 'Plugins: Assist', slug: 'tutorials/assist', key: 'core-concepts/layouts' },
-	{ text: 'Plugins: Fetch', slug: 'tutorials/fetch', key: 'tutorials/layouts' },
-	{ text: 'Plugins: Axios', slug: 'tutorials/axios', key: 'tutorials/layouts' },
-	{ text: 'Plugins: Redux', slug: 'tutorials/redux', key: 'tutorials/layouts' },
-	{ text: 'Plugins: VueX', slug: 'tutorials/vuex', key: 'core-concepts/layouts' },
-	{ text: 'Plugins: Pinia', slug: 'tutorials/pinia', key: 'guides/imports' },
-	{ text: 'Plugins: GraphQL', slug: 'tutorials/graphql', key: 'guides/markdown-content' },
-	{ text: 'Plugins: Zustand', slug: 'tutorials/zustand', key: 'core-concepts/routing' },
-	//features
-	{ text: 'Feature: Omnisearch', slug: 'tutorials/omnisearch', key: 'core-concepts/endpoints' },
-	{ text: 'Feature: Adding users', slug: 'tutorials/adding-users', key: 'tutorials/-fetching' },
-	{ text: 'Feature: Issue types', slug: 'tutorials/issues', key: 'tutorials/' },
-	{ text: 'Feature: Custom events', slug: 'tutorials/custom-events', key: 'tutorials/' },
-	{ text: 'Feature: Metadata', slug: 'tutorials/metadata', key: 'tutorials/' },
-	{ text: 'Feature: Custom dashboards', slug: 'tutorials/custom-dashboard', key: 'tutorials/' },
-	{ text: 'Feature: Custom funnels', slug: 'tutorials/funnels', key: 'tutorials/' },
-	{ text: 'Feature: Custom Metrics - JS errors', slug: 'tutorials/js-errors-dashboard', key: 'tutorials/' },
-	//integrations with frameworks (using openreplay with...)
-	{ text: 'Using OR with Next.js', slug: 'tutorials/next', key: 'tutorials/' },
-	{ text: 'Using OR with Nuxt', slug: 'tutorials/nuxt', key: 'tutorials/' },
-	{ text: 'Using OR with Remix', slug: 'tutorials/remix', key: 'tutorials/' },
-	{ text: 'Using OR with Svelte', slug: 'tutorials/svelte', key: 'tutorials/' },
-	{ text: 'Using OR with Angular', slug: 'tutorials/angular', key: 'tutorials/' },
-	{ text: 'Using OR with Gatsby', slug: 'tutorials/gatsby', key: 'tutorials/' },
-	//extending OpenReplay
-	{ text: 'Creating your first plugin', slug: 'tutorials/build-plugins', key: 'tutorials/troubleshooting' },
-	{ text: 'Creating your first heuristic', slug: 'tutorials/custom-heuristics', key: 'tutorials/troubleshooting' },
+
+	{ text: 'Features', slug: 'installation/', header: true, type: 'learn', key: 'installation', icon: 'installation' },
+	{ text: 'Overview', slug: 'installation', key: 'blog-tutorial' },
+	{ text: 'Custom Events', slug: 'installation/custom-events', key: 'blog-tutorial' },
+	{ text: 'Error reporting', slug: 'installation/error-reporting', key: 'blog-installation' },
+	{ text: 'Identify a User', slug: 'installation/identify-user', key: 'blog-installation' },
+	{ text: 'JavaScript SDK', slug: 'installation/javascript-sdk', key: 'blog-installation' }, 
+	{ text: 'Metadata', slug: 'installation/metadata', key: 'blog-installation' },
+	{ text: 'Sanitize Data', slug: 'installation/sanitize-data', key: 'blog-installation' },
+	{ text: 'Setup OpenReplay', slug: 'installation/setup-or', key: 'blog-installation' },
+	{ text: 'Upload Source maps', slug: 'installation/upload-sourcemaps', key: 'blog-installation' },
 
 	//plugins
 	{ text: 'Plugins', header:true, type: 'learn', slug: 'plugins/', key: 'guides/troubleshooting', icon: 'plugins' },
@@ -94,17 +72,41 @@ export default [
 	{ text: 'Redux', slug: 'plugins/redux', key: 'plugins/troubleshooting' },
 	{ text: 'Zustand', slug: 'plugins/zustand', key: 'plugins/troubleshooting' },
 
+	// //tutorials
+	// { text: 'Tutorials', slug: 'tutorials/', header: true, type: 'learn', key: 'tutorials', icon: 'tutorials' },
+	// //about plugins
+	// { text: 'Overview', slug: 'tutorials', key: 'core-concepts/layouts' },
+	// { text: 'Plugins: Assist', slug: 'tutorials/assist', key: 'core-concepts/layouts' },
+	// { text: 'Plugins: Fetch', slug: 'tutorials/fetch', key: 'tutorials/layouts' },
+	// { text: 'Plugins: Axios', slug: 'tutorials/axios', key: 'tutorials/layouts' },
+	// { text: 'Plugins: Redux', slug: 'tutorials/redux', key: 'tutorials/layouts' },
+	// { text: 'Plugins: VueX', slug: 'tutorials/vuex', key: 'core-concepts/layouts' },
+	// { text: 'Plugins: Pinia', slug: 'tutorials/pinia', key: 'guides/imports' },
+	// { text: 'Plugins: GraphQL', slug: 'tutorials/graphql', key: 'guides/markdown-content' },
+	// { text: 'Plugins: Zustand', slug: 'tutorials/zustand', key: 'core-concepts/routing' },
+	// //features
+	// { text: 'Feature: Omnisearch', slug: 'tutorials/omnisearch', key: 'core-concepts/endpoints' },
+	// { text: 'Feature: Adding users', slug: 'tutorials/adding-users', key: 'tutorials/-fetching' },
+	// { text: 'Feature: Issue types', slug: 'tutorials/issues', key: 'tutorials/' },
+	// { text: 'Feature: Custom events', slug: 'tutorials/custom-events', key: 'tutorials/' },
+	// { text: 'Feature: Metadata', slug: 'tutorials/metadata', key: 'tutorials/' },
+	// { text: 'Feature: Custom dashboards', slug: 'tutorials/custom-dashboard', key: 'tutorials/' },
+	// { text: 'Feature: Custom funnels', slug: 'tutorials/funnels', key: 'tutorials/' },
+	// { text: 'Feature: Custom Metrics - JS errors', slug: 'tutorials/js-errors-dashboard', key: 'tutorials/' },
+	// //integrations with frameworks (using openreplay with...)
+	// // { text: 'Using OR with Next.js', slug: 'tutorials/next', key: 'tutorials/' },
+	// // { text: 'Using OR with Nuxt', slug: 'tutorials/nuxt', key: 'tutorials/' },
+	// // { text: 'Using OR with Remix', slug: 'tutorials/remix', key: 'tutorials/' },
+	// // { text: 'Using OR with Svelte', slug: 'tutorials/svelte', key: 'tutorials/' },
+	// // { text: 'Using OR with Angular', slug: 'tutorials/angular', key: 'tutorials/' },
+	// // { text: 'Using OR with Gatsby', slug: 'tutorials/gatsby', key: 'tutorials/' },
+	// //extending OpenReplay
+	// { text: 'Creating your first plugin', slug: 'tutorials/build-plugins', key: 'tutorials/troubleshooting' },
+	// { text: 'Creating your first heuristic', slug: 'tutorials/custom-heuristics', key: 'tutorials/troubleshooting' },
 
-	//API
-	{ text: 'API (BETA)', header:true,type: 'learn', slug: 'api/', key: 'guides/troubleshooting', icon: 'api' },
-	{ text: 'Overview', slug: 'api', key: 'api/troubleshooting' },
-	{ text: 'Assist', slug: 'api/assist', key: 'api/troubleshooting' },
-	{ text: 'Events', slug: 'api/events', key: 'api/troubleshooting' },
-	{ text: 'Jobs', slug: 'api/jobs', key: 'api/troubleshooting' },
-	{ text: 'Projects', slug: 'api/projects', key: 'api/troubleshooting' },
-	{ text: 'Sessions', slug: 'api/sessions', key: 'api/troubleshooting' },
-	{ text: 'Users', slug: 'api/users', key: 'api/troubleshooting' },
+	
 
+	
 	//integrations
 	{ text: 'Integrations', header:true,type: 'learn', slug: 'integrations/', key: 'guides/troubleshooting', icon: 'integration' },
 	{ text: 'Overview', slug: 'integrations', key: 'integrations/troubleshooting' },
@@ -136,10 +138,22 @@ export default [
 	{ text: 'Supported Browsers', slug: 'troubleshooting/supported-browsers', key: 'troubleshooting/' },
 	{ text: 'Deployment issues', slug: 'troubleshooting/deployment-faq', key: 'troubleshooting/' },
 
-	// structure
-	{ text: 'Structure', header:true,type: 'learn', slug: 'structure/', key: 'structure/troubleshooting', icon: 'structure' },
+// structure
+	{ text: 'Exporting data', header:true,type: 'learn', slug: 'structure/', key: 'structure/troubleshooting', icon: 'structure' },
 	{ text: 'Overview', slug: 'structure', key: 'structure/troubleshooting' },
 	{ text: 'Exported Data', slug: 'structure/exported-data', key: 'structure/troubleshooting' },
 	
-	
+
+
+//API
+{ text: 'API (BETA)', header:true,type: 'learn', slug: 'api/', key: 'guides/troubleshooting', icon: 'api' },
+{ text: 'Overview', slug: 'api', key: 'api/troubleshooting' },
+{ text: 'Assist', slug: 'api/assist', key: 'api/troubleshooting' },
+{ text: 'Events', slug: 'api/events', key: 'api/troubleshooting' },
+{ text: 'Jobs', slug: 'api/jobs', key: 'api/troubleshooting' },
+{ text: 'Projects', slug: 'api/projects', key: 'api/troubleshooting' },
+{ text: 'Sessions', slug: 'api/sessions', key: 'api/troubleshooting' },
+{ text: 'Users', slug: 'api/users', key: 'api/troubleshooting' },
+
+		
 ] as const;
