1b0c6161813a60e2c0e017bed1bd9782cdc8e300
Fernando Doglio <fernando@openreplay.com>
2023-02-20 17:51:13 +0100

    adding React how-to into the tracker setup section

--- src/i18n/en/nav.ts
+++ src/i18n/en/nav.ts
@@ -32,7 +32,7 @@ export default [
 	{ text: 'Tracker Setup', slug: 'using-or/', header: true, type: 'learn', key: 'tracker-setup', icon: 'tracker-setup' },
 	{ text: 'Overview', slug: 'using-or', key: 'tutorials/' },
 	{ text: 'JS Snippet', slug: 'using-or/snippet', key: 'using-or/' },
-	{ text: 'React', slug: 'using-or/next', key: 'using-or/' },
+	{ text: 'React', slug: 'using-or/react', key: 'using-or/' },
 	{ text: 'Vue', slug: 'using-or/next', key: 'using-or/' },
 	{ text: 'Next.js', slug: 'using-or/next', key: 'using-or/' },
 	{ text: 'Nuxt', slug: 'using-or/nuxt', key: 'using-or/' },
