20f3f021cb2a5d7ea43b391dcd821b2898f90ae6
Shekar Siri <sshekarsiri@gmail.com>
2023-04-24 16:24:27 +0200

    version selector - order

--- src/config.ts
+++ src/config.ts
@@ -3,15 +3,15 @@ export const OPEN_GRAPH = {
 };
 
 export const VERSIONS = [
-    {title: "1.5.1", url: "/v1.5.1"},
-    {title: "1.5.2", url: "/v1.5.2"},
-    {title: "1.5.3", url: "/v1.5.3"},
-    {title: "1.5.4", url: "/v1.5.4"},
-    {title: "1.6.0", url: "/v1.6.0"},
-    {title: "1.7.0", url: "/v1.7.0"},
-    {title: "1.8.0", url: "/v1.8.0"},
-    {title: "1.8.1", url: "/v1.8.1"},
-    {title: "1.9.0", url: "/v1.9.0"},
+    {title: "1.11.0", url: ""},
     {title: "1.10.0", url: "/v1.10.0"},
-    {title: "1.11.0", url: ""}
-]
+    {title: "1.9.0", url: "/v1.9.0"},
+    {title: "1.8.1", url: "/v1.8.1"},
+    {title: "1.8.0", url: "/v1.8.0"},
+    {title: "1.7.0", url: "/v1.7.0"},
+    {title: "1.6.0", url: "/v1.6.0"},
+    {title: "1.5.4", url: "/v1.5.4"},
+    {title: "1.5.3", url: "/v1.5.3"},
+    {title: "1.5.2", url: "/v1.5.2"},
+    {title: "1.5.1", url: "/v1.5.1"},
+];
