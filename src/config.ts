01a88ab10ed48794c296754de1b9e892ea3a58e9
estradino <mehdi@openreplay.com>
2023-11-02 22:15:28 -0400

    :star: preparing v1.15.0

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,12 @@ export const OPEN_GRAPH = {
 };
 export const VERSIONS = [
 	{
-		title: '1.14.0',
+		title: '1.15.0',
 		url: '',
+	},
+	{
+		title: '1.14.0',
+		url: '/v1.14.0',
 	},
 	{
 		title: '1.13.0',
@@ -33,9 +37,5 @@ export const VERSIONS = [
 	{
 		title: '1.8.0',
 		url: '/v1.8.0',
-	},
-	{
-		title: '1.7.0',
-		url: '/v1.7.0',
 	}
 ];
