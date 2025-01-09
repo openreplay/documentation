222b83730f7f83eb96bf1c2e98a1e821b9aab4cb
estradino <mehdi@openreplay.com>
2025-01-09 16:11:01 -0500

    Init

--- src/config.ts
+++ src/config.ts
@@ -1,58 +1,61 @@
 export const OPEN_GRAPH = {
-    twitter: "openreplayhq",
-  };
-  export const VERSIONS = [
-    {
-      title: "1.21.0",
-      url: "",
-    },
-    {
-      title: "1.20.0",
-      url: "/v1.20.0",
-    },
-    {
-      title: "1.19.0",
-      url: "/v1.19.0",
-    },
-    {
-      title: "1.18.0",
-      url: "/v1.18.0",
-    },
-    {
-      title: "1.17.0",
-      url: "/v1.17.0",
-    },
-    {
-      title: "1.16.0",
-      url: "/v1.16.0",
-    },
-    {
-      title: "1.15.0",
-      url: "/v1.15.0",
-    },
-    {
-      title: "1.14.0",
-      url: "/v1.14.0",
-    },
-    {
-      title: "1.13.0",
-      url: "/v1.13.0",
-    },
-    {
-      title: "1.12.0",
-      url: "/v1.12.0",
-    },
-    {
-      title: "1.11.0",
-      url: "/v1.11.0",
-    },
-    {
-      title: "1.10.0",
-      url: "/v1.10.0",
-    },
-    {
-      title: "1.9.0",
-      url: "/v1.9.0",
-    },
-  ];
-  
+	twitter: 'openreplayhq',
+};
+export const VERSIONS = [
+	{
+		title: '1.22.0',
+		url: '',
+	},
+	{
+		title: '1.21.0',
+		url: '/v1.21.0',
+	},
+	{
+		title: '1.20.0',
+		url: '/v1.20.0',
+	},
+	{
+		title: '1.19.0',
+		url: '/v1.19.0',
+	},
+	{
+		title: '1.18.0',
+		url: '/v1.18.0',
+	},
+	{
+		title: '1.17.0',
+		url: '/v1.17.0',
+	},
+	{
+		title: '1.16.0',
+		url: '/v1.16.0',
+	},
+	{
+		title: '1.15.0',
+		url: '/v1.15.0',
+	},
+	{
+		title: '1.14.0',
+		url: '/v1.14.0',
+	},
+	{
+		title: '1.13.0',
+		url: '/v1.13.0',
+	},
+	{
+		title: '1.12.0',
+		url: '/v1.12.0',
+	},
+	{
+		title: '1.11.0',
+		url: '/v1.11.0',
+	},
+	{
+		title: '1.10.0',
+		url: '/v1.10.0',
+	},
+	{
+		title: '1.9.0',
+		url: '/v1.9.0',
+	},
+];
