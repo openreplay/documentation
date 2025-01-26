9244c374fcd87cef10d84ca663862dd39ac1ba4a
Ghaida Bouchaala <ghaida.bouchaala@gmail.com>
2025-01-26 15:16:17 +0100

    add visuals and restructured folders

--- src/i18n/en/nav.ts
+++ src/i18n/en/nav.ts
@@ -20,7 +20,7 @@ const nav: NavItem = {
       isSectionTitle: true
     },
     {
-      text: 'What is OpenReplay?',
+      text: 'Home',
       slug: 'home',
       icon: 'home',
       hideChevron: true,
@@ -30,11 +30,11 @@ const nav: NavItem = {
       text: 'Getting started',
       slug: 'getting-started',
       icon: 'start',
-      hideChevron: false,
+      hideChevron: true,
       children: [
-        { text: 'OpenReplay Serverless', slug: 'getting-started/serverless', children: [] },
-        { text: 'OpenReplay Dedicated', slug: 'getting-started/dedicated', children: [] },
-        { text: 'OpenReplay Self-Host', slug: 'getting-started/self-host', children: [] },
+        // { text: 'OpenReplay Serverless', slug: 'getting-started/serverless', children: [] },
+        // { text: 'OpenReplay Dedicated', slug: 'getting-started/dedicated', children: [] },
+        // { text: 'OpenReplay Self-Host', slug: 'getting-started/self-host', children: [] },
       ]
     },
     {
@@ -69,10 +69,15 @@ const nav: NavItem = {
           { text: 'OpenReplay CLI', slug: 'deployment/openreplay-admin', children: []},
           { text: 'Upgrade Deployment', slug: 'deployment/upgrade', children: [] }
         ] },
-        { text: 'Administration', slug: null, children: [
-          { text: 'Team Management', slug: 'administration/invite-team-members', children: [] },
-          { text: 'Roles and Access', slug: 'administration/roles-and-access', children: [] }
+        { text: 'Installation', slug: null, children: [
+          { text: 'Setup OpenReplay', slug: 'deployment/setup-or', children: [] },
+          { text: 'Upload Source Maps', slug: 'deployment/upload-sourcemaps', children: [] },
         ] },
+        { text: 'Administration', slug: null, children: [
+          { text: 'Team Management', slug: 'deployment/invite-team-members', children: [] },
+          { text: 'Roles and Access', slug: 'deployment/roles-and-access', children: [] }
+        ] },
+        
       ]
     },
     {
@@ -105,18 +110,41 @@ const nav: NavItem = {
       children: [
         { text: 'Overview', slug: 'sdk', children: [] },
         { text: 'Initialization', slug: 'sdk/constructor', children: [] },
-        { text: 'Methods', slug: 'sdk/methods', children: [] },
+        { text: 'Methods', slug: 'sdk/methods', children: [
+          { text: 'event', slug: 'sdk/methods/event' },
+          { text: 'getSessionToken', slug: 'sdk/methods/get-session-token', children: [] },
+          { text: 'getSessionID', slug: 'sdk/methods/get-session-id', children: [] },
+          { text: 'getSessionURL', slug: 'sdk/methods/get-session-url', children: [] },
+          { text: 'handleError', slug: 'sdk/methods/handle-error', children: [] },
+          { text: 'isActive', slug: 'sdk/methods/is-active', children: [] },
+          { text: 'issue', slug: 'sdk/methods/issue', children: [] },
+          { text: 'setUserID', slug: 'sdk/methods/set-user-id', children: [] },
+          { text: 'setUserAnonymousID', slug: 'sdk/methods/set-user-anonymous-id', children: [] },
+          { text: 'setMetadata', slug: 'sdk/methods/set-metadata', children: [] },
+          { text: 'isFlagEnabled', slug: 'sdk/methods/is-flag-enabled', children: [] },
+          { text: 'reloadFags', slug: 'sdk/methods/reload-flags', children: [] },
+          { text: 'getFeatureFlag', slug: 'sdk/methods/get-feature-flag', children: [] },
+          { text: 'getAllFeatureFlags', slug: 'sdk/methods/get-all-feature-flags', children: [] },
+          { text: 'clearPersistFlag', slug: 'sdk/methods/clear-persist-flag', children: [] },
+          { text: 'start', slug: 'sdk/methods/start', children: [] },
+          { text: 'stop', slug: 'sdk/methods/stop', children: [] },
+          { text: 'coldStart', slug: 'sdk/methods/cold-start', children: [] },
+          { text: 'startOfflineRecording', slug: 'sdk/methods/start-offline-recording', children: [] },
+          { text: 'uploadOfflineRecording', slug: 'sdk/methods/upload-offline-recording', children: [] },
+          { text: 'forceFlushBatch', slug: 'sdk/methods/force-flush-batch', children: [] },
+          { text: 'trackWs', slug: 'sdk/methods/track-ws', children: [] }, 
+        ] },
         { text: 'Network Options', slug: 'sdk/network-options', children: [] },
         { text: 'Sanitize Data', slug: 'sdk/sanitize-data', children: [] },
-        { text: 'Frameworks', slug: 'null', children: [
-          { text: 'React', slug: 'using-or/react', children: [] },
-          { text: 'Vue', slug: 'using-or/vue', children: [] },
-          { text: 'Next.js', slug: 'using-or/next', children: [] },
-          { text: 'Nuxt', slug: 'using-or/nuxt', children: [] },
-          { text: 'Remix', slug: 'using-or/remix', children: [] },
-          { text: 'Svelte', slug: 'using-or/svelte', children: [] },
-          { text: 'Angular', slug: 'using-or/angular', children: [] },
-          { text: 'Gatsby', slug: 'using-or/gatsby', children: [] },  
+        { text: 'Frameworks', slug: 'sdk/using-or/', children: [
+          { text: 'React', slug: 'sdk/using-or/react', children: [] },
+          { text: 'Vue', slug: 'sdk/using-or/vue', children: [] },
+          { text: 'Next.js', slug: 'sdk/using-or/next', children: [] },
+          { text: 'Nuxt', slug: 'sdk/using-or/nuxt', children: [] },
+          { text: 'Remix', slug: 'sdk/using-or/remix', children: [] },
+          { text: 'Svelte', slug: 'sdk/using-or/svelte', children: [] },
+          { text: 'Angular', slug: 'sdk/using-or/angular', children: [] },
+          { text: 'Gatsby', slug: 'sdk/using-or/gatsby', children: [] },  
         ] },
       ]
     },
@@ -127,9 +155,17 @@ const nav: NavItem = {
       children: [
         { text: 'Initialization', slug: 'ios-sdk/init', children: [] },
         { text: 'Modules', slug: 'ios-sdk/modules', children: [] },
-        { text: 'Methods', slug: 'ios-sdk/methods', children: [] },
-        { text: 'Network Options', slug: 'ios-sdk/network-options', children: [] },
-        { text: 'Sanitize Data', slug: 'ios-sdk/sanitize-data', children: [] },
+        { text: 'Methods', slug: 'ios-sdk/methods', children: [
+          { text: 'event', slug: 'ios-sdk/methods/event' , children: []},
+          { text: 'setUserID', slug: 'ios-sdk/methods/set-user-id', children: [] },
+          { text: 'userAnonymousID', slug: 'ios-sdk/methods/user-anonymous-id', children: [] },
+          { text: 'setMetadata', slug: 'ios-sdk/methods/set-metadata', children: [] },
+          { text: 'start', slug: 'ios-sdk/methods/start', children: [] },
+          { text: 'stop', slug: 'ios-sdk/methods/stop', children: [] },
+        ] },
+        { text: 'Inputs', slug: 'ios-sdk/inputs', children: [] },     
+        { text: 'Network Options', slug: 'ios-sdk/network', children: [] },
+        { text: 'Sanitize Data', slug: 'ios-sdk/sanitization', children: [] },
       ]
     },
     {
@@ -139,9 +175,16 @@ const nav: NavItem = {
       children: [
         { text: 'Initialization', slug: 'android-sdk/init', children: [] },
         { text: 'Modules', slug: 'android-sdk/modules', children: [] },
-        { text: 'Methods', slug: 'ios-sdk/methods', children: [] },
-        { text: 'Network Options', slug: 'ios-sdk/network-options', children: [] },
-        { text: 'Sanitize Data', slug: 'ios-sdk/sanitize-data', children: [] },
+        { text: 'Methods', slug: 'android-sdk/methods', children: [
+          { text: 'event', slug: 'android-sdk/methods/event' , children: []},
+          { text: 'setUserID', slug: 'android-sdk/methods/set-user-id', children: [] },
+          { text: 'userAnonymousID', slug: 'android-sdk/methods/user-anonymous-id', children: [] },
+          { text: 'setMetadata', slug: 'android-sdk/methods/set-metadata', children: [] },
+          { text: 'start', slug: 'android-sdk/methods/start', children: [] },
+        ] },
+        { text: 'Inputs', slug: 'android-sdk/inputs', children: [] },        
+        { text: 'Network Options', slug: 'android-sdk/network', children: [] },
+        { text: 'Sanitize Data', slug: 'android-sdk/sanitization', children: [] },
       ]
     },
     {
@@ -151,9 +194,17 @@ const nav: NavItem = {
       children: [
         { text: 'Initialization', slug: 'rn-sdk/init', children: [] },
         { text: 'Modules', slug: 'rn-sdk/modules', children: [] },
-        { text: 'Methods', slug: 'rn-sdk/methods', children: [] },
-        { text: 'Network Options', slug: 'rn-sdk/network-options', children: [] },
-        { text: 'Sanitize Data', slug: 'rn-sdk/sanitize-data', children: [] },
+        { text: 'Methods', slug: 'rn-sdk/methods', children: [
+          { text: 'event', slug: 'rn-sdk/methods/event' , children: []},
+          { text: 'setUserID', slug: 'rn-sdk/methods/set-user-id', children: [] },
+          { text: 'userAnonymousID', slug: 'rn-sdk/methods/user-anonymous-id', children: [] },
+          { text: 'setMetadata', slug: 'rn-sdk/methods/set-metadata', children: [] },
+          { text: 'start', slug: 'rn-sdk/methods/start', children: [] },
+          { text: 'patchNetwork', slug: 'rn-sdk/methods/patch-network', children: [] },
+        ] },
+        { text: 'Inputs', slug: 'rn-sdk/inputs', children: [] },        
+        { text: 'Network Options', slug: 'rn-sdk/network', children: [] },
+        { text: 'Sanitize Data', slug: 'rn-sdk/sanitization', children: [] },
       ]
     },
     {
@@ -170,17 +221,18 @@ const nav: NavItem = {
       icon: 'session-replay',
       children: [
         { text: 'Overview', slug: 'session-replay', children: [] },
-        { text: 'Guides', slug: 'null', children: [
+        { text: 'Guides', slug: null, children: [ 
           { text: 'Omnisearch', slug: 'session-replay/omnisearch', children: [] },
           { text: 'Highlights', slug: 'session-replay/highlights', children: [] },
         ] 
         },
-        { text: 'Advanced Setup', slug: 'null', children: [
-          { text: 'Identify a User', slug: 'session-replay/omnisearch', children: [] },
+        { text: 'Advanced Setup', slug: null, children: [
+          { text: 'Identify a User', slug: 'session-replay/identify-user', children: [] },
           { text: 'Send Metadata', slug: 'session-replay/metadata', children: [] },
-          { text: 'Canvas & WebGL', slug: 'session-replay/canvas-webgl', children: [] },
-          { text: 'Cross-domain iFrames', slug: 'session-replay/cross-domain-iframes', children: [] },
+          { text: 'Canvas and WebGL', slug: 'session-replay/canvas', children: [] },
+          { text: 'Cross-domain iFrames', slug: 'session-replay/crossdomain-iframe', children: [] },
           { text: 'WebSockets', slug: 'session-replay/websockets', children: [] },
+          { text: 'Error Tracking', slug: 'session-replay/error-reporting', children: [] },
         ] 
         },
       ]
@@ -191,27 +243,24 @@ const nav: NavItem = {
       icon: 'product-analytics',
       children: [
         { text: 'Overview', slug: 'product-analytics', children: [] },
-        { text: 'Guides', slug: 'null', children: [
-          { text: 'Dashboards', slug: 'product-analytics/dashboards', children: [] },
+        { text: 'Guides', slug: null, children: [
           { text: 'Trends', slug: 'product-analytics/trends', children: [] },
           { text: 'Funnels', slug: 'product-analytics/funnels', children: [] },
           { text: 'Journeys', slug: 'product-analytics/journeys', children: [] },
           { text: 'Heatmaps', slug: 'product-analytics/heatmaps', children: [] },
+          { text: 'Dashboards', slug: 'product-analytics/dashboards', children: [] },
           { text: 'Web Analytics', slug: 'product-analytics/web-analytics', children: [] },
-          { text: 'Monitors', slug: 'product-analytics/Monitors', children: [] },
-          { text: 'Custom Events', slug: 'product-analytics/custom-events', children: [] },
-
+          { text: 'Monitors', slug: 'product-analytics/monitors', children: [] },
+        ]
+        },
+        { text: 'Advanced Setup', slug: null, children: [
+          { text: 'Custom Events', slug: 'product-analytics/custom-events', children: [] }
         ] 
         },
       ]
     },
     {
-      text: 'Co-browsing',
-      slug: 'co-browsing',
-      icon: 'co-browsing',
-      children: [
-        { text: 'Overview', slug: 'co-browsing', children: [] }
-      ]
+      text: 'Co-browsing', slug: 'co-browsing', icon: 'co-browsing', children: []
     },
     {
       text: 'Extensions',
@@ -309,6 +358,22 @@ const nav: NavItem = {
         { text: 'Sourcemaps Issues', slug: 'troubleshooting/sourcemaps', children: [] },
         { text: 'Supported Browsers', slug: 'troubleshooting/supported-browsers', children: [] },
         { text: 'Deployment Issues', slug: 'troubleshooting/deployment-issues', children: [] },
+      ]
+    },
+    {
+      text: 'Tutorials',
+      slug: null,
+      icon: 'tutorials',
+      children: [
+        { text: 'Assist', slug: 'tutorials/assist', children: [] },
+        { text: 'Build Plugins', slug: 'tutorials/build-plugins', children: [] },
+        { text: 'Custom Events', slug: 'tutorials/custom-events', children: [] },
+        { text: 'Capture and Sanitize', slug: 'tutorials/capture-request', children: [] },
+        { text: 'GraphQL', slug: 'tutorials/graphql', children: [] },
+        { text: 'Metadata', slug: 'tutorials/metadata', children: [] },
+        { text: 'Redux', slug: 'tutorials/redux', children: [] },
+        { text: 'VueX', slug: 'tutorials/vuex', children: [] },
+        { text: 'Zustand', slug: 'tutorials/zustand', children: [] },
       ]
     },
     {
