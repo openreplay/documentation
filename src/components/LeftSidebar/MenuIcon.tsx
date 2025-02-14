efa0f7aa59122978e94b0e75d2187a58293d8ad6
Ghaida Bouchaala <ghaida.bouchaala@gmail.com>
2025-02-14 11:45:43 +0100

    add troubleshooting to spot and visuals

--- src/components/LeftSidebar/MenuIcon.tsx
+++ src/components/LeftSidebar/MenuIcon.tsx
@@ -219,7 +219,7 @@ const MenuIcon = (props) => {
 			<circle cx="12" cy="12" r="1" fill="none"></circle>
 			</svg>;
 		case 'ai':
-		    return  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles bg-gradient-to-r from-teal-500 bg-indigo-600 hover:bg-blue-700 cursor-pointer text-white font-bold drop-shadow-sm rounded-md">
+		    return  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles  hover:bg-gradient-to-r  cursor-pointer font-bold drop-shadow-sm rounded-md">
 				<path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/>
 				<path d="M20 3v4"/><path d="M22 5h-4"/><path d="M4 17v2"/><path d="M5 18H3"/>
 			</svg>
